84031f9af2e4476e1168d6f41b206952f6cf731d
kholishoh <54460129+kholishoh@users.noreply.github.com>
2020-04-17 20:58:08 +0700

    Add files via upload

---
+++ MPI/01.mpi_p2p_terbesar.py
@@ -0,0 +1,23 @@
+# import mpi4py
+from mpi4py import MPI
+
+# buat COMM
+comm = MPI.COMM_WORLD
+
+# dapatkan rank proses
+rank = comm.Get_rank()
+
+# dapatkan total proses berjalan
+size = comm.Get_size()
+
+# jika saya rank terbesar maka saya akan mengirimkan pesan ke proses yang mempunyai rank 0 s.d rank terbesar-1
+if rank == 1:
+	data = "Pesan"
+	for i in range (0,size):
+                comm.send(data, dest=i, tag=i)
+                print("rank :", rank, "dengan data :", data, "destination :",i)
+
+# jika saya bukan rank terbesar maka saya akan menerima pesan yang berasal dari proses dengan rank terbesar
+else:
+	data = comm.recv(source = size-1)
+	print("rank :", rank, "dengan data :", data)
---
+++ MPI/03.sum_p2p.py
@@ -0,0 +1,33 @@
+# import mpi4py
+from mpi4py import MPI
+
+# import library random untuk generate angka integer secara random
+import random
+
+# buat COMM
+comm = MPI.COMM_WORLD
+
+# dapatkan rank proses
+rank = comm.Get_rank()
+
+# dapatkan total proses berjalan
+size = comm.Get_size()
+
+# generate angka integer secara random untuk setiap proses
+generate = random.randint(1,5)
+
+# jika saya adalah proses dengan rank 0 maka:
+# saya menerima nilai dari proses 1 s.d proses dengan rank terbesar
+# menjumlah semua nilai yang didapat (termasuk nilai proses saya)
+if rank == 0:
+    sum = 0
+    for i in range (1,size):
+        data = comm.recv(source=i, tag=rank)
+        print(data)
+        sum += data['send']
+    print("sum :", sum)
+	
+# jika bukan proses dengan rank 0, saya akan mengirimkan nilai proses saya ke proses dengan rank=0
+else:
+        data = {"rank" : rank, "destination" : 0, "send" : generate}
+	comm.send(data, dest=0, tag=0)
